329b28bd5f3832131717ba08de4bcb678c6d437f
mereluo <meredithluo710@gmail.com>
2023-02-27 19:08:06 +0000

    Implement initialization

--- queue.c
+++ queue.c
@@ -14,7 +14,11 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    // return null pointer if the size for malloc is 0
+    if (head != NULL)
+        INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
